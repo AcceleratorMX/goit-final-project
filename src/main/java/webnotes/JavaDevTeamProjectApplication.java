231c4be0aea3bed97d09a08a82e6acf6328e2ecc
AcceleratorMX <redfield242@gmail.com>
2023-08-13 08:59:06 +0300

    Final

---
+++ src/main/java/webnotes/JavaDevTeamProjectApplication.java
@@ -0,0 +1,12 @@
+package webnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevTeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevTeamProjectApplication.class, args);
+	}
+}
